e202b4e22142899da2a695ea5d1c36a29f62f389
SeldereyTM <55454980+SeldereyTM@users.noreply.github.com>
2020-07-04 11:22:14 +0300

    Create Экзамен3.cpp

---
+++ Экзамен3.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Counter
+{
+public:
+	Counter(int sec)
+	{
+		seconds = sec;
+	}
+	void display()
+	{
+		std::cout << seconds << " seconds" << std::endl;
+	}
+	int seconds;
+};
+
+Counter operator + (Counter c1, Counter c2)
+{
+	return Counter(c1.seconds + c2.seconds);
+}
+
+int main()
+{
+	Counter c1(20);
+	Counter c2(10);
+	Counter c3 = c1 + c2;
+	c3.display();   // 30 seconds
+
+	return 0;
+}
